e4ad643a8c8b49167ff2d8800d815d40eb7bb2c1
PetkoDrenkov <petko727150@gmail.com>
2026-05-10 19:06:02 +0300

    Update  SOLVING CASES IN SQLPD 1.sql

--- part2/ SOLVING CASES IN SQLPD 1.sql
+++ part2/ SOLVING CASES IN SQLPD 1.sql
@@ -14,5 +14,36 @@ FROM subscribers;
 SELECT Username, MailingAddress
 FROM members;
 
+-- Case 5. 
+SELECT *
+FROM users 
+ORDER BY FamilyName ASC;
+
+-- Case 6.
+SELECT *
+FROM members
+ORDER BY NumberOfComments DESC;
+
+-- Case 7.
+SELECT DISTINCT Name, SubscribedSince
+FROM subscribers 
+ORDER BY SubscribedSince DESC;
+
+-- Case 8.
+SELECT NumberOfKids, FamilyName
+FROM mailing_list
+ORDER BY NumberOfKids ASC,  FamilyName ASC;
+
+-- Case 9.
+SELECT *
+FROM subscribers
+ORDER BY Comments ASC, Name DESC
+LIMIT 3;
+
+-- Case 10.
+SELECT DISTINCT NumberOfDownloads, FamilyName, LastAccess
+FROM users
+ORDER BY FamilyName ASC, LastAccess DESC
+LIMIT 3;
 
 
